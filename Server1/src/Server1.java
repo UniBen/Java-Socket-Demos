08d12806da564679a71367b54091e5eeb7d437f9
Gary Allen <g.allen@hud.ac.uk>
2019-10-01 16:05:18 +0100

    Minor formatting fixes

--- Server1/src/Server1.java
+++ Server1/src/Server1.java
@@ -12,69 +12,69 @@ import java.net.*;
 import java.io.*;
 
 public class Server1 {
-        public static void main(String args[]) {
-        SimpleServer ss = new SimpleServer(6001) ;
-    }        
+	public static void main(String args[]) {
+		SimpleServer ss = new SimpleServer(6001) ;
+	}
 } // class Server1
 
 class SimpleServer {
-    ServerSocket sock;
-    Socket conn;
-    
-    BufferedReader instream;
-    BufferedWriter outstream;
-    String str;
-    
-    //  establish server socket and listen for client 
-    SimpleServer(int port) {
-	try {
-	    sock = new ServerSocket(port);
-	    System.out.println("Started on port " + port);
+	ServerSocket sock;
+	Socket conn;
 
-	    conn = sock.accept();       //  listen for client connection
-	}
-	catch(Exception e) {
-	    System.out.println("Error : " + e);
-	    System.exit(1);
-	}
-	
-	//  socket between client and server now established	
-	//     socket has input and output streams asssociated with it
-	try {
-	    instream = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-	} catch (Exception e) {
-	    System.out.println("Error : " + e);
-	    System.out.println("Can't get input stream for socket");
-	    System.exit(1);
-	}
+	BufferedReader instream;
+	BufferedWriter outstream;
+	String str;
 
-	try {
-	    outstream = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
-	} catch (Exception e) {
-	    System.out.println("Error : " + e);
-	    System.out.println("Can't get output stream for socket");
-	    System.exit(1);
-	}
-       
-	//  read all client data from socket until "bye" is sent
-	do {
-	    try {
-		// wait for client to send data
-		str = instream.readLine();;         
+	//  establish server socket and listen for client
+	SimpleServer(int port) {
+		try {
+			sock = new ServerSocket(port);
+			System.out.println("Started on port " + port);
 
-		// send a reply to client
-		outstream.write("You said " + str); 
-		outstream.newLine();
-		outstream.flush();
-	    } catch(IOException e) {
-		System.out.println("Error : " + e);
-		System.exit(1);
-	    }	    
+			conn = sock.accept();       //  listen for client connection
+		}
+		catch(Exception e) {
+			System.out.println("Error : " + e);
+			System.exit(1);
+		}
 
-	    System.out.println("Client sent string " + str.trim());
-	}  while ( !str.startsWith("bye") );
-	
-	System.out.println("Closing server now");
-	System.exit(0);
-    }  // SimpleServer constructor    
+		//  socket between client and server now established
+		//     socket has input and output streams asssociated with it
+		try {
+			instream = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		} catch (Exception e) {
+			System.out.println("Error : " + e);
+			System.out.println("Can't get input stream for socket");
+			System.exit(1);
+		}
+
+		try {
+			outstream = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+		} catch (Exception e) {
+			System.out.println("Error : " + e);
+			System.out.println("Can't get output stream for socket");
+			System.exit(1);
+		}
+
+		//  read all client data from socket until "bye" is sent
+		do {
+			try {
+				// wait for client to send data
+				str = instream.readLine();;
+
+				// send a reply to client
+				outstream.write("You said " + str);
+				outstream.newLine();
+				outstream.flush();
+			} catch(IOException e) {
+				System.out.println("Error : " + e);
+				System.exit(1);
+			}
+
+			System.out.println("Client sent string " + str.trim());
+		}  while ( !str.startsWith("bye") );
+
+		System.out.println("Closing server now");
+		System.exit(0);
+	}  // SimpleServer constructor
 } // class SimpleServer
--- Server4/src/Server4.java
+++ Server4/src/Server4.java
@@ -11,132 +11,132 @@
 import java.net.*;
 import java.io.*;
 
-public class Server4 {     
-    public static void main(String args[]) {
-	
-	int port;
-	
-	try {
-	    port = Integer.parseInt(args[0]);
-	} catch (Exception e) {
-	    port = 6001;
+public class Server4 {
+	public static void main(String args[]) {
+
+		int port;
+
+		try {
+			port = Integer.parseInt(args[0]);
+		} catch (Exception e) {
+			port = 6001;
+		}
+
+		MultiThreadServer mts = new MultiThreadServer(port);
 	}
-	
-	MultiThreadServer mts = new MultiThreadServer(port);
-    }        
 } // class Server4
 
 
 class MultiThreadServer {
 
-    ServerSocket sock;
-    Socket conn;
-    int port;
-   
-    //  establish server socket and listen for client 
-    MultiThreadServer(int port) {
-	this.port = port;
-	
-	try {
-	    sock = new ServerSocket(port);
-	    System.out.println("Server started on port " + port); 
-	} catch(Exception e) {
-	    System.out.println("Error : " + e);
-	    System.out.println("Can't start server on port " + port);
-	    System.exit(1);
-	}
+	ServerSocket sock;
+	Socket conn;
+	int port;
 
-	while (true) {
-	    try {
-		conn = sock.accept();       //  listen for client connection
-		ClientHandler ch = new ClientHandler(conn);
-		System.out.println("new client connection");
-	    } catch(Exception e) {
-		System.out.println("Error : " + e);
-		System.out.println("Can't create socket for client");
-		System.exit(1);
-	    }
-	}
-    } // end MultiThreadServer constructor
+	//  establish server socket and listen for client
+	MultiThreadServer(int port) {
+		this.port = port;
+
+		try {
+			sock = new ServerSocket(port);
+			System.out.println("Server started on port " + port);
+		} catch(Exception e) {
+			System.out.println("Error : " + e);
+			System.out.println("Can't start server on port " + port);
+			System.exit(1);
+		}
+
+		while (true) {
+			try {
+				conn = sock.accept();       //  listen for client connection
+				ClientHandler ch = new ClientHandler(conn);
+				System.out.println("new client connection");
+			} catch(Exception e) {
+				System.out.println("Error : " + e);
+				System.out.println("Can't create socket for client");
+				System.exit(1);
+			}
+		}
+	} // end MultiThreadServer constructor
 }  // end class MultiThreadedServer
 
 
 class ClientHandler implements Runnable {
-    Socket conn;
-    Thread clientThread;
-    
-    BufferedReader instream; 
-    BufferedWriter outstream;
-    
-    String str;
-    
-    ClientHandler(Socket connin) {
-	conn = connin;
-	clientThread = new Thread(this);
-	clientThread.start();
-    } // end ClientHandler constructor
+	Socket conn;
+	Thread clientThread;
 
-    public void stop() {
-	try {
-	    conn.close();
-	}
-	catch(Exception e) {
-	    System.out.println("Error : " + e);
-	}
-	clientThread = null;
-    } // end method stop
-    
-    public void run() {
-	try {
-	    instream = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-	} catch (Exception e) {
-	    System.out.println("Error : " + e);
-	    System.out.println("Can't get input stream for socket");
-	    System.exit(1);
-	}
+	BufferedReader instream;
+	BufferedWriter outstream;
 
-	try {
-	    outstream = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
-	} catch (Exception e) {
-	    System.out.println("Error : " + e);
-	    System.out.println("Can't get output stream for socket");
-	    System.exit(1);
-	}
+	String str;
 
-	try {
-	    outstream.write("Your wish is my command . . .");
-	    outstream.newLine();
-	    outstream.flush();
-	} catch(IOException e) {
-	    System.out.println("Error : " + e);
-	    System.exit(1);
-	}
-	
-	do {
-	    try {
-		str = instream.readLine();           // wait for client to send data   
-		outstream.write("You said " + str);  // send a reply to client
-		outstream.newLine();
-		outstream.flush();
-      
-		System.out.print("Client sent string " + str.trim());           
-		System.out.println(" on socket " + conn.toString());
-	    } catch(IOException e) {
-		System.out.println("Error : " + e);
-		System.exit(1);
-	    }
-	}  while ( !str.startsWith("bye") );
+	ClientHandler(Socket connin) {
+		conn = connin;
+		clientThread = new Thread(this);
+		clientThread.start();
+	} // end ClientHandler constructor
 
-	try { 
-	    outstream.write("Missing you already . . ");
-	    outstream.newLine();
-	    outstream.flush();
-	} catch(IOException e) {
-	    System.out.println("Error : " + e);
-	    System.exit(1);
-	}
+	public void stop() {
+		try {
+			conn.close();
+		}
+		catch(Exception e) {
+			System.out.println("Error : " + e);
+		}
+		clientThread = null;
+	} // end method stop
 
-	System.out.println("closing client connecton");
-	this.stop();
-    }  // end method run   
+	public void run() {
+		try {
+			instream = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		} catch (Exception e) {
+			System.out.println("Error : " + e);
+			System.out.println("Can't get input stream for socket");
+			System.exit(1);
+		}
+
+		try {
+			outstream = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+		} catch (Exception e) {
+			System.out.println("Error : " + e);
+			System.out.println("Can't get output stream for socket");
+			System.exit(1);
+		}
+
+		try {
+			outstream.write("Your wish is my command . . .");
+			outstream.newLine();
+			outstream.flush();
+		} catch(IOException e) {
+			System.out.println("Error : " + e);
+			System.exit(1);
+		}
+
+		do {
+			try {
+				str = instream.readLine();           // wait for client to send data
+				outstream.write("You said " + str);  // send a reply to client
+				outstream.newLine();
+				outstream.flush();
+
+				System.out.print("Client sent string " + str.trim());
+				System.out.println(" on socket " + conn.toString());
+			} catch(IOException e) {
+				System.out.println("Error : " + e);
+				System.exit(1);
+			}
+		}  while ( !str.startsWith("bye") );
+
+		try {
+			outstream.write("Missing you already . . ");
+			outstream.newLine();
+			outstream.flush();
+		} catch(IOException e) {
+			System.out.println("Error : " + e);
+			System.exit(1);
+		}
+
+		System.out.println("closing client connecton");
+		this.stop();
+	}  // end method run
 } // class ClientHandler
